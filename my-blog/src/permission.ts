a0dd818263278952931261d8dfdcdae9d8f370e3
WsmDyj <972774037@qq.com>
2019-12-18 10:50:18 +0800

    新增--nprogress功能

--- my-blog/src/permission.ts
+++ my-blog/src/permission.ts
@@ -1,10 +1,18 @@
 import router from './router'
 import { Route } from 'vue-router'
+
+import NProgress from 'nprogress'
+
+import 'nprogress/nprogress.css'
+
 import { UserModule } from './store/modules/user'
 import { ArticleModule } from './store/modules/article'
 import { followsModule } from './store/modules/follow'
 
+NProgress.configure({ showSpinner: false })
+
 router.beforeEach(async(to: Route, from: Route, next: any) => {
+  NProgress.start()
   if (UserModule.token) {
     // 获取用户的信息
     await UserModule.GetUserInfo() 
@@ -14,9 +22,11 @@ router.beforeEach(async(to: Route, from: Route, next: any) => {
     await followsModule.getFollows() 
   }
   next()
+  NProgress.done()
 })
 router.afterEach((to: Route) => {
   // set page title
+  NProgress.done()
   if (to.meta.title) {
     document.title = to.meta.title
   }
