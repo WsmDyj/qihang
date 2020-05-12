c9e1eaf1bd953306ff8a680aa99492157e470ea4
WsmDyj <972774037@qq.com>
2020-05-12 12:18:54 +0800

    优化--github登录

--- my-blog/src/views/oauth/index.vue
+++ my-blog/src/views/oauth/index.vue
@@ -4,7 +4,7 @@ import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 @Component
 export default class  extends Vue {
   created() {
-    window.location.href = 'https://github.com/login/oauth/authorize?client_id=745084b69bda83213a3b&redirect_uri=http://qihang.pro/redirect'
+    window.location.href = 'https://github.com/login/oauth/authorize?client_id=745084b69bda83213a3b&redirect_uri=http://qihang.pro/'
   }
 }
 </script>
